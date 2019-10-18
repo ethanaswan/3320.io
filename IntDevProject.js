887501bbe6d2f368359755fe96d96e829804dad6
ethanaswan <55563652+ethanaswan@users.noreply.github.com>
2019-10-18 11:32:35 -0500

    Add files via upload

--- IntDevProject.js
+++ IntDevProject.js
@@ -24,13 +24,26 @@ function totalDue(var price, var tax; var shippingCost) {
 function validateCardNum(var cardNum) {
 	for (var i = 0; i < 15; i++) {
 	if ()
- }
+	}
 }
 
 function validateMmYyyy(var expDate) {
 	var today = new Date();
 	var month = (today.getMonth() + 1);
 	var year = today.getFullYear();
-	
+	if (expDate[2] != "/")
+		return false;
+	if (expDate[3]+expDate[4]+expDate[5]+expDate[6] < year)
+		return false;
+	else if(expDate[3]+expDate[4]+expDate[5]+expDate[6] == year) {
+		if (expDate[0] + expDate[1] < month)
+			return false;
+	}
+	else
+		return true;
 }	
+
+function submitOrder() {
+
+}
 
