64edd9e1ef79994bf5dc64588d123e4521f3427d
ethanaswan <55563652+ethanaswan@users.noreply.github.com>
2019-10-18 10:37:58 -0500

    Add files via upload

--- IntDevProject.js
+++ IntDevProject.js
@@ -1,6 +1,14 @@
-<!DOCTYPE html>
-<html>
-<body>
+function calculate() {
+	double totalPrice = 0.0;
+	for (int i = 0; i < numChosen; i++) {
+	totalPrice = 
+ }
+}
 
-</body>
-</html>
+function totalTax(double price) {
+	double tax = 0, totalPrice = 0;
+	tax = (price * .08);
+	totalPrice = price + tax;
+	return totalPrice;
+
+}
